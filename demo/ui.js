535a95ba84f7f4b287d5a1d24fd518c5b5353e77
Vincent De Oliveira <vincent@iamvdo.me>
2016-02-05 18:12:14 +0100

    :art: Add duration control

--- demo/ui.js
+++ demo/ui.js
@@ -1,35 +1,33 @@
-// sticky title
-stickyElements('h1 span');
-// sticky links
-stickyElements('a', {stickiness: {x: 2, y: 5}});
 // sticky inputs[range]
 stickyElements('[type=range]', {stickiness: {x: 1, y: 5}});
 
 var inputs = [].slice.call(document.querySelectorAll('[type=range]'));
 inputs.forEach(function (input) {
   updateTrack(input);
-  updateStickiness();
+  updateOptions();
   input.addEventListener('input', updateTrack.bind(this, input), false);
-  input.addEventListener('change', updateStickiness.bind(this, input), false);
+  input.addEventListener('change', updateOptions.bind(this, input), false);
 });
 
-function updateStickiness (input) {
+function updateOptions (input) {
   if (input) {
     hintButton('.button--primary');
   }
   var inputX = Math.ceil(document.getElementById('inputX').value);
   var inputY = Math.ceil(document.getElementById('inputY').value);
-  stickyElements('.button', {
+  var inputDuration = document.getElementById('inputDuration').value;
+  stickyElements('.button, a, h1 span', {
     stickiness: {
       x: inputX,
       y: inputY
-    }
+    },
+    duration: inputDuration
   });
 }
 
 function updateTrack (input) {
   var id = input.id;
-  var value = 100 - (input.value*10);
+  var value = 100 - (input.value*100/input.getAttribute('max'));
   var factor = Number(((1 - (value / 100)) * 2) - 1).toFixed(2);
   var val = 'calc(' + value + '% + (' + factor + ' * (1.25em / 2))) 100%, 100% 100%';
 
