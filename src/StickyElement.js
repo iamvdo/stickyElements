b2d1adef2afa7455e834538e213127d4d2bd14f9
Vincent De Oliveira <vincent@iamvdo.me>
2016-02-18 17:59:05 +0100

    Deal with events while animations occur on element

--- src/StickyElement.js
+++ src/StickyElement.js
@@ -76,14 +76,16 @@ export default class StickyElement {
     this.stickiness.y = forces[Math.min(10, opt.stickiness.y)];
   }
 
-  getPositions (deltax, deltay) {
-    const posx = (this.stickiness.x !== 0) ? deltax / this.stickiness.x : 0;
-    const posy = (this.stickiness.y !== 0) ? deltay / this.stickiness.y : 0;
+  getPositions () {
+    const posx = (this.stickiness.x !== 0) ? this.positions.deltax / this.stickiness.x : 0;
+    const posy = (this.stickiness.y !== 0) ? this.positions.deltay / this.stickiness.y : 0;
     return {posx, posy};
   }
 
   onEnter (event) {
-    const {offsetWidth, offsetHeight, offsetLeft, offsetTop} = this.el;
+    this.state = 'ENTER';
+    const element = this.el;
+    const {offsetWidth, offsetHeight, offsetLeft, offsetTop} = element;
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const positions = {
       width: offsetWidth,
@@ -92,54 +94,86 @@ export default class StickyElement {
       centery: offsetTop + (offsetHeight / 2) - scrollTop
     };
     this.positions = positions;
-  }
-
-  onLeave (event) {
-    // prevent mouseleave event to be triggered twice by 30ms
-    if (this.lastGripped) {
-      const now = new Date().getTime();
-      if (now - this.lastGripped < 30) {
-        return;
-      }
-    }
-    const element = this.el;
-    animate.stop(element);
-
-    const {posx, posy} = this.getPositions(this.positions.deltax, this.positions.deltay);
-
-    if (this.isGripped) {
-      this.lastGripped = new Date().getTime();
-      animate({
-        el: element,
-        translateX: [posx, 0],
-        translateY: [posy, 0],
-        duration: this.duration
-      });
-    }
     this.isGripped = false;
   }
 
+  onLeave (event) {
+    if (this.state === 'MOVE') {
+      this.state = 'LEAVE';
+      const element = this.el;
+      const {x, y} = this.getTranslate(element);
+      animate.stop(element);
+      animate({
+        el: element,
+        translateX: [x, 0],
+        translateY: [y, 0],
+        duration: this.duration
+      });
+      this.isGripped = false;
+    }
+  }
+
   onMove (event) {
+    this.state = 'MOVE';
     const element = this.el;
     animate.stop(element);
 
     const {clientX, clientY} = event;
-
     const isGrip = {
       x: Math.abs(this.positions.deltax) < (this.positions.width /  this.grip.x),
       y: Math.abs(this.positions.deltay) < (this.positions.height / this.grip.y)
     };
-
     if (isGrip.x && isGrip.y) {
       this.isGripped = true;
     }
-
     this.positions.deltax = -(this.positions.centerx - clientX);
     this.positions.deltay = -(this.positions.centery - clientY);
 
     if (this.isGripped) {
-      const {posx, posy} = this.getPositions(this.positions.deltax, this.positions.deltay);
+      const {posx, posy} = this.getPositions();
       element.style.transform = 'translate3d(' + posx + 'px, ' + posy + 'px, 0)';
     }
+  }
+
+  getTranslate (elem) {
+    const val = getComputedStyle(elem).transform;
+    const matrix = this.parseMatrix(val);
+    return {
+      x: matrix.m41,
+      y: matrix.m42,
+      z: matrix.m43
+    };
+  }
+
+  parseMatrix (matrixString) {
+    const c = matrixString.split(/\s*[(),]\s*/).slice(1,-1);
+    let matrix;
+
+    if (c.length === 6) {
+        // 'matrix()' (3x2)
+        matrix = {
+            m11: +c[0], m21: +c[2], m31: 0, m41: +c[4],
+            m12: +c[1], m22: +c[3], m32: 0, m42: +c[5],
+            m13: 0,     m23: 0,     m33: 1, m43: 0,
+            m14: 0,     m24: 0,     m34: 0, m44: 1
+        };
+    } else if (c.length === 16) {
+        // matrix3d() (4x4)
+        matrix = {
+            m11: +c[0], m21: +c[4], m31: +c[8],  m41: +c[12],
+            m12: +c[1], m22: +c[5], m32: +c[9],  m42: +c[13],
+            m13: +c[2], m23: +c[6], m33: +c[10], m43: +c[14],
+            m14: +c[3], m24: +c[7], m34: +c[11], m44: +c[15]
+        };
+    } else {
+        // handle 'none' or invalid values.
+        matrix = {
+            m11: 1, m21: 0, m31: 0, m41: 0,
+            m12: 0, m22: 1, m32: 0, m42: 0,
+            m13: 0, m23: 0, m33: 1, m43: 0,
+            m14: 0, m24: 0, m34: 0, m44: 1
+        };
+    }
+    return matrix;
   }
 }
