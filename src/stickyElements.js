54b1c8da702f96b462bbc1cfcd1ce89bdc4c098b
Medet Tleukabiluly ✎ <m.diddy_11@mail.ru>
2016-04-24 02:56:53 +0600

    Remove redundant operations on array
    ES2015 way looks much cleaner tho =)

--- src/stickyElements.js
+++ src/stickyElements.js
@@ -2,11 +2,7 @@ import StickyElement from './StickyElement';
 
 export default (() => {
   return (selector, opts) => {
-    const stickyElementsTab = [];
     const elements = [].slice.call(document.querySelectorAll(selector));
-    for (let i = 0; i < elements.length; i++) {
-      stickyElementsTab.push(new StickyElement(elements[i], opts));
-    };
-    return stickyElementsTab;
+    return elements.map((e) => new StickyElement(e, opts));
   };
 })();
